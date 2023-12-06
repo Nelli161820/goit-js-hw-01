7b0843fd1df45821b1303ae461e999bc096cd70f
Nelli161820 <124123144+Nelli161820@users.noreply.github.com>
2023-12-06 19:37:51 +0400

    js_hw_1a

--- js/task-1.js
+++ js/task-1.js
@@ -2,12 +2,11 @@
 
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
-}
-makeTransaction(5, 3000);
-makeTransaction(3, 1000);
-makeTransaction(10, 500);
+  const transaction = (`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  return transaction;
+};
 
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log(makeTransaction(5, 3000)); 
+console.log(makeTransaction(3, 1000)); 
+console.log(makeTransaction(10, 500)); 
+
